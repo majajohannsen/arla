33106075be7bb7e03f0642dd710e3eb252cf8733
majahoberg <eaamhob@students.eaaa.dk>
2020-11-05 15:04:40 +0100

    noget

---
+++ javascript/firebase_config.js
@@ -0,0 +1,18 @@
+"use strict";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const _firebaseConfig = {
+    apiKey: "AIzaSyA2tgrFrgD_lynuFZe1pEAIvyj5VuWO2mw",
+    authDomain: "arla-efda1.firebaseapp.com",
+    databaseURL: "https://arla-efda1.firebaseio.com",
+    projectId: "arla-efda1",
+    storageBucket: "arla-efda1.appspot.com",
+    messagingSenderId: "125864658422",
+    appId: "1:125864658422:web:301bfaf5d676ae19e033d7",
+    measurementId: "G-QS3H91LYXL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(_firebaseConfig);
+  const _db = firebase.firestore();
+
+  
