240e650e604c8a1c628d0774291ab42567272e4c
majahoberg <eaamhob@students.eaaa.dk>
2020-11-05 13:26:04 +0100

    firebase

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,19 @@
 "use strict";
 // Your web app's Firebase configuration
-const _firebaseConfig = {
-  apiKey: "AIzaSyCMyBxTqrwDePj6kSVgD5rVsEQ-66FCoCk",
-  authDomain: "mdu-e18front.firebaseapp.com",
-  databaseURL: "https://mdu-e18front.firebaseio.com",
-  projectId: "mdu-e18front",
-  storageBucket: "mdu-e18front.appspot.com",
-  messagingSenderId: "1065294705229",
-  appId: "1:1065294705229:web:81f00c89d44d800c75e204"
-};
-// Initialize Firebase and database references
-firebase.initializeApp(_firebaseConfig);
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  let _firebaseConfig = {
+    apiKey: "AIzaSyA2tgrFrgD_lynuFZe1pEAIvyj5VuWO2mw",
+    authDomain: "arla-efda1.firebaseapp.com",
+    databaseURL: "https://arla-efda1.firebaseio.com",
+    projectId: "arla-efda1",
+    storageBucket: "arla-efda1.appspot.com",
+    messagingSenderId: "125864658422",
+    appId: "1:125864658422:web:301bfaf5d676ae19e033d7",
+    measurementId: "G-QS3H91LYXL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(_firebaseConfig);
+  firebase.analytics();
 const _db = firebase.firestore();
 
 
